5230d803b1c798ad4a16a85ae33cee7e31796fd9
unknown <zolrak.614@gmail.com>
2022-04-20 11:36:24 -0500

    first commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2"
+       @click="$router.push({ name: 'entry', params: {id:10}})"
+  >
+      <div class="entry-title d-flex">
+          <!-- Título -->
+          <span class="text-success fs-5 fw-bold">18</span>
+          <span class="mx-1 fs-5">abril</span>
+          <span class="mx-2 fw-light">2022, lunes</span>
+      </div>
+
+      <div class="entry-description">
+          “Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?”
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+    <div>
+      <span class="text-success fs-3 fw-bold">18</span>
+      <span class="mx-1 fs-3">abril</span>
+      <span class="mx-2 fs-4 fw-light">2022, lunes</span>
+    </div>
+
+    <div>
+      <button class="btn btn-danger mx-2">
+        Borrar 
+        <i class="fa fa-trash-alt"></i>
+      </button>
+
+      <button class="btn btn-primary">
+        Subir foto
+        <i class="fa fa-upload"></i>
+      </button>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea
+      placeholder="¿Qué sucede hoy?"
+    ></textarea>
+  </div>
+
+  <Fab 
+    icon="fa-save"
+  />
+
+  <img 
+    src="@/assets/eren.jpg" 
+    alt="entry-picture"
+    class="img-thubnail Erenimg"> 
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ img {
+   width: 200px;
+   position: fixed;
+   bottom: 150px;
+   right: 20px;
+   box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+ }
+
+ textarea {
+   font-size: 20px;
+   border: none;
+   height: 100%;
+
+   &:focus {
+     outline: none;
+   }
+ }
+</style>
